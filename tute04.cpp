99420a14eeedeafdf54cc1e92b8fc6197df60fdd
Jayavi <87510354+it21042638@users.noreply.github.com>
2021-07-20 08:54:39 +0000

    Tutorial 2 completed

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,22 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no)
+{
+  long fac = 1;
+    for (int r=no; r >= 1; r--) {
+        fac = fac * r;
+    }
+    return fac;
+}
+
+long nCr(int n, int r)
+{
+  int N = Factorial(n);
+  int R = Factorial(r);
+  int sub = Factorial(n-r);
+
+  return N/(R*sub);
+  
+}
